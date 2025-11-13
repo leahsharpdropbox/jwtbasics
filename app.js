efb5569ffc90fdd930c9d16b33e584090f2fa023
leahsharpdropbox <leahsharpdropbox@gmail.com>
2025-11-13 14:12:10 +0000

    .

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const app = express();
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const connectDB = require('./db/connect');
+const mainRouter = require('./routes/main')
 
 // middleware
 app.use(express.static('./public'));
 app.use(express.json());
 
+
+app.use('/api/v1', mainRouter)
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
---
+++ controllers/main.js
@@ -0,0 +1,36 @@
+const jwt = require("jsonwebtoken")
+require('dotenv').config()
+const CustomAPIError = require("../errors/custom-error")
+
+const login = async (req, res) => {
+    const { username, password } = req.body
+    if (!username || !password) {
+        throw new CustomAPIError('Please provide username and password', 400)
+    }
+    const id = new Date().getDate()
+    const token = jwt.sign({ username, id }, process.env.JWT_SECRET, {expiresIn: '30d'})
+    console.log(username, password, token)
+    res.status(200).json({ msg: 'user created', token})
+}
+
+const dashboard = async (req, res) => {
+    const authHeader = req.headers.authorization
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
+        throw new CustomAPIError('No token provided', 401)
+    }
+    const token = authHeader.split(' ')[1]
+    console.log(token)
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        console.log(decoded)
+        const luckyNumber = Math.floor(Math.random() * 100)
+        res.status(200).json({ msg: `Hello, ${decoded.username}`, secret: `Here is your authorized data, your lucky number is ${luckyNumber}` })
+    } catch (error) {
+        throw new CustomAPIError('Not authorized to access this route', 401)
+    }
+}
+
+module.exports = {
+    login,
+    dashboard
+}
